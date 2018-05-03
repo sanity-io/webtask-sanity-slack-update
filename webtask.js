88f780877df30b54a672b0456907bf5b324ada98
Knut Melvær <knut.melvaer@netlife.com>
2018-05-03 23:00:08 +0200

    tweak copy

--- webtask.js
+++ webtask.js
@@ -12,30 +12,30 @@ const client = sanityClient({
 
 module.exports = async function(context, cb) {
   const { created, updated, deleted } = context.body.ids;
-  const query = async ids => client.fetch(`*[_id in [${ids.map(JSON.stringify).join(',')}]]{title,name}`);
+  const query = async ids => client.fetch(`*[_id in [${ids.map(JSON.stringify).join(',')}]]{title,name,_type}`);
   const data = {
-    nytt: created.length && await query(created),
-    oppdatert: updated.length && await query(updated),
-    slettet: deleted.length && await query(deleted)
+    created: created.length && await query(created),
+    updated: updated.length && await query(updated),
+    deleted: deleted.length && await query(deleted)
   };
   console.log(JSON.stringify(data, undefined, 2))
   const { SLACK_WEBHOOK_URL } = context.secrets
   const messages = payload => Object.keys(payload).filter(key => payload[key]).map(key => ({
-    title: key,
+    title: `Something was ${key} in ${payload[key]._type}:`,
     value: payload[key].map(({ title, name }) => name ? name : title.nb).join(', '),
     short: true
   }))
   console.log(JSON.stringify(messages(data), undefined, 2))
   const options = {
-    text: "Noko skjedde i Netlife Sanityen!!",
+    text: "Something happened in Sanity!",
     attachments: [{ fields: messages(data) }]
   };
   console.log(JSON.stringify(options, undefined, 2))
   return axios.post(SLACK_WEBHOOK_URL, JSON.stringify(options)).then((response) => {
     console.log('SUCCEEDED: Sent slack webhook: \n', response.data);
-    cb(null, { hello: context.query.name || 'Anonymous' });
+    cb(null, 200);
   }).catch((error) => {
     console.log('FAILED: Send slack webhook', error);
-    cb(null, { hello: context.query.name || 'Anonymous' });
+    cb(null, 500);
   });
 };
