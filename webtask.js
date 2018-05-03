ec8274b63c73ad961f1276fa92d38e4ad52a5162
Knut Melvær <knut.melvaer@netlife.com>
2018-05-03 23:29:04 +0200

    remove unnecessary stuff

--- webtask.js
+++ webtask.js
@@ -1,33 +1,30 @@
 /**
 * @param context {WebtaskContext}
 */
-require('babel-polyfill');
 const axios = require('axios');
 const sanityClient = require('@sanity/client')
 const client = sanityClient({
-    projectId: 'l3oee2le',
-    dataset: 'production',
+    projectId: '<YOUR PROJECT ID>',
+    dataset: '<YOUR DATASET NAME>',
     useCdn: true,
 });
 
 module.exports = async function(context, cb) {
   const { created, updated, deleted } = context.body.ids;
-  const query = async ids => client.fetch(`*[_id in [${ids.map(JSON.stringify).join(',')}]]{title,name,_type}`);
+  const query = async ids => client.fetch(`*[_id in [${ids.map(JSON.stringify).join(',')}]]{title,_type}`);
   const data = {
     created: created.length && await query(created),
     updated: updated.length && await query(updated),
     deleted: deleted.length && await query(deleted)
   };
-  console.log(JSON.stringify(data, undefined, 2))
   const { SLACK_WEBHOOK_URL } = context.secrets
   const messages = payload => Object.keys(payload).filter(key => payload[key]).map(key => ({
-    title: `Something was ${key} in ${payload[key]._type}:`,
-    value: payload[key].map(({ title, name }) => name ? name : title.nb).join(', '),
+    title: `Something was ${key} in ${payload[key].map(({_type}) => _type).join(', ')}:`,
+    value: payload[key].map(({ title, name }) => name ? name : title).join(', '),
     short: true
   }))
-  console.log(JSON.stringify(messages(data), undefined, 2))
   const options = {
-    text: "Something happened in Sanity!",
+    text: "Something happened in Sanity! :eyes:",
     attachments: [{ fields: messages(data) }]
   };
   console.log(JSON.stringify(options, undefined, 2))
